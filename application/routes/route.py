df9a274b0f36b5f1ebed9c0a2dffe197da4c5e50
Bhushan Dhammrakshit <bhushand@maqsoftware.com>
2025-07-03 21:53:47 +0530

    Added signup page

--- application/routes/route.py
+++ application/routes/route.py
@@ -2,8 +2,12 @@ from application import app
 from flask import render_template
 
 @app.route("/login")
-def index():
-    return render_template('index.html')
+def logIn():
+    return render_template('logIn.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
 
 @app.route("/layout")
 def layout():
